e8a21a9b062d4e5fc3137771aed54ce7878aee09
du-java <79944381+du-java@users.noreply.github.com>
2021-11-11 19:40:54 +0100

    add TC (#1)

--- src/test/java/by/karpov/shop/ShopApplicationTests.java
+++ src/test/java/by/karpov/shop/ShopApplicationTests.java
@@ -2,14 +2,12 @@ package by.karpov.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class ShopApplicationTests {
+class ShopApplicationTests extends AbstractIT {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
